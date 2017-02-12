3bb1018ed2960631b0a220e88eddbaff8530f767
swirlingsand <anthonysarkis@gmail.com>
2017-02-11 23:58:41 -0600

    Updates from Udacity to model server connection file

--- p3-behavioral-cloning-project/drive.py
+++ p3-behavioral-cloning-project/drive.py
@@ -1,80 +1,68 @@
 import argparse
 import base64
-import json
+from datetime import datetime
+import os
+import shutil
 
 import numpy as np
 import socketio
 import eventlet
 import eventlet.wsgi
-import time
 from PIL import Image
-from PIL import ImageOps
-from flask import Flask, render_template
+from flask import Flask
 from io import BytesIO
 
 from keras.models import load_model
-from keras.models import model_from_json
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
-
-import matplotlib.pyplot as plt
-import time
-
-# Fix error with Keras and TensorFlow
-import tensorflow as tf
-tf.python.control_flow_ops = tf
-
+import h5py
+from keras import __version__ as keras_version
 
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
 
-steering_angles = []
-
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
-    # The current throttle of the car
-    throttle = data["throttle"]
-    # The current speed of the car
-    speed = data["speed"]
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
+        # The current speed of the car
+        speed = data["speed"]
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image_array = np.asarray(image)
 
-    """ Pre Processing """
-    # new_size_col, new_size_row  = 1,1
+        top = int(.40 * image_array.shape[0])
+        bottom = int(.1 * image_array.shape[0])
+        image_array = image_array[top:-bottom, :]
 
-    # Normalize
-    # image_array = image_array / 255.
+        steering_angle = float(model.predict(
+            image_array[None, :, :, :], batch_size=1))
 
-    """ End 
-    """
-    top = int(.40 * image_array.shape[0])
-    bottom = int(.1 * image_array.shape[0])
-    image_array = image_array[top:-bottom, :]
-    #print("shape", image_array.shape)
+        min_speed = 8
+        max_speed = 25
+        if float(speed) < min_speed:
+            throttle = 1.0
+        elif float(speed) > max_speed:
+            throttle = -1.0
+        else:
+            throttle = 0.2
 
-    transformed_image_array = image_array[None, :, :, :]
-    # This model currently assumes that the features of the model are just the
-    # images. Feel free to change this.
-    steering_angle = float(model.predict(
-        transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free
-    # to edit this.
-    # Added control statement based on steering angle.
-    throttle = 0.2
+        print(steering_angle, throttle)
+        send_control(steering_angle, throttle)
 
-    if steering_angle > .1:
-        throttle = .15
-
-    steering_angles.append(steering_angle)
-
-    print(steering_angle, throttle)
-    send_control(steering_angle, throttle)
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+    else:
+        # NOTE: DON'T EDIT THIS.
+        sio.emit('manual', data={}, skip_sid=True)
 
 
 @sio.on('connect')
@@ -84,44 +72,55 @@ def connect(sid, environ):
 
 
 def send_control(steering_angle, throttle):
-    sio.emit("steer", data={
-        'steering_angle': steering_angle.__str__(),
-        'throttle': throttle.__str__()
-    }, skip_sid=True)
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument('model', type=str,
-                        help='Path to model definition json. Model weights should be on the same path.')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
     args = parser.parse_args()
-    with open(args.model, 'r') as jfile:
-        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
-        # then you will have to call:
-        #
-        #model = model_from_json(json.loads(jfile.read()))\
-        #
-        # instead.
-        #model = model_from_json(jfile.read())
-        model = load_model('my_model.h5')
 
-    model.compile("adam", "mse")
-    weights_file = args.model.replace('json', 'h5')
-    model.load_weights(weights_file)
+    # check that model Keras version is same as local Keras version
+    f = h5py.File(args.model, mode='r')
+    model_version = f.attrs.get('keras_version')
+    keras_version = str(keras_version).encode('utf8')
+
+    if model_version != keras_version:
+        print('You are using Keras version ', keras_version,
+              ', but the model was built using ', model_version)
+
+    model = load_model(args.model)
+
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
-
-    wait_time = 30
-    time.sleep(wait_time)
-    # Plot
-    Plt_number = np.random.randint(0, 1000)
-    plt.plot(steering_angles)
-    plt.title('Angles')
-    plt.ylabel('')
-    plt.xlabel('')
-    plt.show()
-    plt.savefig('angles-drive-history' + str(Plt_number) + '.png')
