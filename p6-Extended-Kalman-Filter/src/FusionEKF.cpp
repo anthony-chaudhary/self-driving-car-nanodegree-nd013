7ad8656d9eef0116de45d614b10fa900995b83b2
swirlingsand <anthonysarkis@gmail.com>
2017-04-19 14:09:16 -0600

    add timestamp update and improve efficiency

--- p6-Extended-Kalman-Filter/src/FusionEKF.cpp
+++ p6-Extended-Kalman-Filter/src/FusionEKF.cpp
@@ -24,8 +24,6 @@ FusionEKF::FusionEKF() {
   H_laser_ << 1, 0, 0, 0,
               0, 1, 0, 0;
 
-  // Hj_ = MatrixXd(3, 4);
-
   //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
               0,      0.0225;
@@ -41,8 +39,7 @@ FusionEKF::FusionEKF() {
               0,    0,    1000,   0,
               0,    0,    0,    1000;
 
-  // state transistion matrix
-  // will change with time
+  // state transistion matrix will change with time
   ekf_.F_ = MatrixXd(4, 4);
   ekf_.F_ << 1, 0, 0, 0,
              0, 1, 0, 0,
@@ -50,7 +47,6 @@ FusionEKF::FusionEKF() {
              0, 0, 0, 1;
 
   ekf_.Q_ = MatrixXd(4, 4);
-  // appears to need this to run otherwise get nan(ind) error
   ekf_.Q_ << 0, 0, 0, 0,
              0, 0, 0, 0,
              0, 0, 0, 0,
@@ -115,34 +111,35 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   // 1) compute elapsed time where delta time (dt) == difference_time, expresssed in seconds
   double secondsDivisor = 1000000.0; // 1,000,000
   double dt = (measurement_pack.timestamp_ - previous_timestamp_) / secondsDivisor;
-
-  // Pre computer time difference power terms 
-  // where dt_x == "_x" == power term ie dt_3 == dt to the third power
-  double dt_2 = dt * dt;
-  double dt_3 = dt_2 * dt;
-  double dt_4 = dt_3 * dt;
-  // end precomputation of dt terms
-
-  // 2) use Delta t to compute new EKF F, Q
-
-  // modfy F matrix to integrate time
-  ekf_.F_(0, 2) = dt;
-  ekf_.F_(1, 3) = dt;
-
-  // acceleration noise components for Q matrix.
-  const int noise_ax = 9;
-  const int noise_ay = 9;
-
-    // set covariance matrix Q
-  ekf_.Q_ = MatrixXd(4, 4);
-  ekf_.Q_ <<  dt_4/4*noise_ax, 0,               dt_3/2*noise_ax, 0,
-              0,               dt_4/4*noise_ay, 0,               dt_3/2*noise_ay,
-              dt_3/2*noise_ax, 0,               dt_2*noise_ax,   0,
-              0,               dt_3/2*noise_ay, 0,               dt_2*noise_ay;
-
+  long long previous_timestamp_ = measurement_pack.timestamp_;
 
   // If time difference is acceptable do prediction step. 
   if (dt > .001) {
+
+    // Pre computer time difference power terms 
+    // where dt_x == "_x" == power term ie dt_3 == dt to the third power
+    double dt_2 = dt * dt;
+    double dt_3 = dt_2 * dt;
+    double dt_4 = dt_3 * dt;
+    // end precomputation of dt terms
+
+    // 2) use Delta t to compute new EKF F, Q
+
+    // modfy F matrix to integrate time
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
+
+    // acceleration noise components for Q matrix.
+    const int noise_ax = 9;
+    const int noise_ay = 9;
+
+      // set covariance matrix Q
+    ekf_.Q_ = MatrixXd(4, 4);
+    ekf_.Q_ <<  dt_4/4*noise_ax, 0,               dt_3/2*noise_ax, 0,
+                0,               dt_4/4*noise_ay, 0,               dt_3/2*noise_ay,
+                dt_3/2*noise_ax, 0,               dt_2*noise_ax,   0,
+                0,               dt_3/2*noise_ay, 0,               dt_2*noise_ay;
+
     ekf_.Predict();
   }
 
@@ -153,11 +150,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   // Setup matrices
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
 
-    // Lineraize measurement function
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
-
     // Radar updates
     ekf_.R_ = R_radar_;
+
+    // Lineraize measurement function
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
 
     // State update with new measurements
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
