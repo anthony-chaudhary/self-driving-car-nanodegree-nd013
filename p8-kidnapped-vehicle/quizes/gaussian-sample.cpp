14ab46fc242eae76ccb0114a7f375716b860dd2d
swirlingsand <anthonysarkis@gmail.com>
2017-05-03 14:11:32 -0600

    complete quiz

--- p8-kidnapped-vehicle/quizes/gaussian-sample.cpp
+++ p8-kidnapped-vehicle/quizes/gaussian-sample.cpp
@@ -21,25 +21,24 @@ void printSamples(double gps_x, double gps_y, double psi) {
 	default_random_engine gen;
 	double std_x, std_y, std_psi; // Standard deviations for x, y, and psi
 
-	// TODO: Set standard deviations for x, y, and psi.
-	 
-	
+	// Set standard deviations for x, y, and psi.
+	std_x 	= 2 ;
+	std_y	= 2 ;
+	std_psi = .05 ;
 	
 	// This line creates a normal (Gaussian) distribution for x.
-	normal_distribution<double> dist_x(gps_x, std_x);
-	
-	// TODO: Create normal distributions for y and psi.
-	
-	
+	normal_distribution<double> dist_x(gps_x, std_x) ;
+	normal_distribution<double> dist_y(gps_y, std_y) ;
+	normal_distribution<double> dist_psi(psi, std_psi) ;
 
 	for (int i = 0; i < 3; ++i) {
 		double sample_x, sample_y, sample_psi;
 		
-		// TODO: Sample  and from these normal distrubtions like this: 
-		// sample_x = dist_x(gen);
-		// where "gen" is the random engine initialized earlier (line 18).
-
-		
+		// Sample from these normal distrubtions
+		// where "gen" is the random engine initialized earlier (line 21).
+		sample_x = dist_x(gen) ;
+		sample_y = dist_y(gen) ;
+		sample_psi = dist_psi(gen) ;
 		
 		// Print your samples to the terminal.
 		cout << "Sample " << i + 1 << " " << sample_x << " " << sample_y << " " << sample_psi << endl;
