85704e91cfac6ad6f09caa9751ec35e0d44eda3b
swirlingsand <anthonysarkis@gmail.com>
2017-05-05 19:40:13 -0600

    FOUND THE BUG
    `weight_placeholder *= part_1 * exp( (-1/2.) * (part_2));`  is
    `!=`
    ```long double exp_part_2        = exp( (-1/2) * (part_2))
    ;
    weight_placeholder *= part_1 * exp_part_2       ;```

--- p8-kidnapped-vehicle/src/particle_filter.cpp
+++ p8-kidnapped-vehicle/src/particle_filter.cpp
@@ -13,7 +13,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Initialize all particles to first position (based on estimates of 
 	//   x, y, theta and their uncertainties from GPS)
 
-	num_particles = 2;
+	num_particles = 300;
 
 	random_device rd;
     mt19937 gen(rd());
@@ -23,7 +23,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	normal_distribution<double> dist_y(		y, 		std[1]) ;
 	normal_distribution<double> dist_psi(	theta, 	std[2]);
 
-	weights.resize(num_particles) ;
+	// weights.resize(num_particles) ;
 	particles.resize(num_particles) ;
 
 	for (int i = 0; i < num_particles; i++) {
@@ -87,105 +87,104 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 }
 
 
-
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
-		std::vector<LandmarkObs> observations, Map map_landmarks) {
+    std::vector<LandmarkObs> observations, Map map_landmarks) {
 
 
-	weights.clear();
+  weights.clear();
 
-	// cout << "Starting update weights" << endl;
-	for (int i = 0; 	i < num_particles; 		i++) {
+  // cout << "Starting update weights" << endl;
+  for (int i = 0;   i < num_particles;    i++) {
 
-		// key note, all of this is for 1 particle, ie a particle easily may have 5+ obersvations
-		// 1. Convert particle observations to global frame
-		// cout << transformed_observations.size() << endl;
+    // key note, all of this is for 1 particle, ie a particle easily may have 5+ obersvations
+    // 1. Convert particle observations to global frame
+    // cout << transformed_observations.size() << endl;
 
-		// 2. Only keep landmarks that are within range
-		// reasonable_landmarks 	= landmarks obverserved by particles, that are also within sensor range
-		// vector<LandmarkObs> reasonable_landmarks ;
-		// reasonable_landmarks = ParticleFilter::reasonable_landmarks(transformed_observations, map_landmarks, sensor_range) ;
-		// 3. Create list of landmark ids associated with observations
-		// vector<int> transformed_observations_landmark_ids;
-		//transformed_observations_landmark_ids = ParticleFilter::dataAssociation(map_landmarks, transformed_observations) ;
+    // 2. Only keep landmarks that are within range
+    // reasonable_landmarks   = landmarks obverserved by particles, that are also within sensor range
+    // vector<LandmarkObs> reasonable_landmarks ;
+    // reasonable_landmarks = ParticleFilter::reasonable_landmarks(transformed_observations, map_landmarks, sensor_range) ;
+    // 3. Create list of landmark ids associated with observations
+    // vector<int> transformed_observations_landmark_ids;
+    //transformed_observations_landmark_ids = ParticleFilter::dataAssociation(map_landmarks, transformed_observations) ;
 
-		// 4. Weight distribution
-		const double standard_x 			= std_landmark[0] ;
-		const double standard_y 			= std_landmark[1] ;
-		const double standard_x_squared_2	= (standard_x * standard_x) ;
-		const double standard_y_squared_2	= (standard_y * standard_y) ;
-		const long double part_1 			= 1 / (2 * M_PI * standard_x * standard_y) ;
-		// cout << part_1 << endl;
-		//cout << reasonable_landmarks.size() << "\t" << transformed_observations.size() << endl;
+    // 4. Weight distribution
+    const double standard_x       = std_landmark[0] ;
+    const double standard_y       = std_landmark[1] ;
+    const double standard_x_squared_2 = (standard_x * standard_x) ;
+    const double standard_y_squared_2 = (standard_y * standard_y) ;
+    const long double part_1      = 1 / (2 * M_PI * standard_x * standard_y) ;
+    // cout << part_1 << endl;
+    //cout << reasonable_landmarks.size() << "\t" << transformed_observations.size() << endl;
 
-		long double weight_placeholder = 1.0 ;
-		// if reasonable landmarks have been found
-		// cout << reasonable_particles_landmark_ids.size() << "\t" << transformed_observations.size() << endl;
-		for (unsigned int j = 0; 	j < observations.size();	j++) {
-	
-			// Create placeholder for transformations
-			vector<LandmarkObs> transformed_observations ;
-			transformed_observations.resize(observations.size()) ;
+    long double weight_placeholder = 1.0 ;
+    // if reasonable landmarks have been found
+    // cout << reasonable_particles_landmark_ids.size() << "\t" << transformed_observations.size() << endl;
+    for (unsigned int j = 0;  j < observations.size();  j++) {
+  
+      const double particle_theta = particles[i].theta ;
+      const double cos_theta    = cos(particle_theta) ;
+      const double sin_theta    = sin(particle_theta) ; 
+      // p_x + (obs_x * cos(theta)) - (obs_y * sin(theta))
+      double transformed_observation_x =  particles[i].x   + (observations[j].x * cos_theta) - 
+                          (observations[j].y * sin_theta) ;
+      // p_y + (obs_x * sin(theta)) + (obs_y * cos(theta))             
+      double transformed_observation_y =  particles[i].y   + (observations[j].x * sin_theta) + 
+                         (observations[j].y * cos_theta) ;
+      double threshold = 50.0 ;
+      long current_id = -1;
 
-			// Pre-compute terms
-			const double particle_theta = particles[i].theta ;
-			const double cos_theta 		= cos(particle_theta) ;
-			const double sin_theta 		= sin(particle_theta) ; 
+      for (unsigned int q = 0;  q < map_landmarks.landmark_list.size(); q++) {
 
-			// p_x + (obs_x * cos(theta)) - (obs_y * sin(theta))
-			double transformed_observation_x =  particles[i].x   + (observations[j].x * cos_theta) - 
-											    (observations[j].y * sin_theta) ;
-			// p_y + (obs_x * sin(theta)) + (obs_y * cos(theta))					   
-			double transformed_observation_y =  particles[i].y   + (observations[j].x * sin_theta) + 
-											   (observations[j].y * cos_theta) ;
+        // Find the predicted measurement that is closest to each observed measurement
+        double difference = dist( transformed_observation_x, transformed_observation_y,
+          map_landmarks.landmark_list[q].x_f,    map_landmarks.landmark_list[q].y_f ) ;
+        // check if threshold is less than previous
+        if (difference < threshold) {
+          // if it is, then we have the current lowest ID
+          current_id = map_landmarks.landmark_list[q].id_i -1 ;
+          // assign the threshold to the current difference so that next loop if the difference is higher it won't update the id.
+          threshold = difference ;
+          }
+      }
 
-			double threshold = 50.0 ;
-			int current_id = -1;
+      // cout << current_id << endl ;
 
-			for (unsigned int q = 0; 	q < map_landmarks.landmark_list.size();	q++) {
+      if (current_id >= 0) {
 
-				// Find the predicted measurement that is closest to each observed measurement
-				double difference = dist( map_landmarks.landmark_list[q].x_f,    map_landmarks.landmark_list[q].y_f, 
-								   			transformed_observation_x, transformed_observation_y) ;
-				// check if threshold is less than previous
-				if (difference < threshold) {
-					// if it is, then we have the current lowest ID
-					current_id = map_landmarks.landmark_list[q].id_i - 1;
-					// assign the threshold to the current difference so that next loop if the difference is higher it won't update the id.
-					threshold = difference ;
-				}
-				if (current_id >= 0) {
+        const long double x         = transformed_observation_x ;
+        const long double u_x         = map_landmarks.landmark_list[current_id].x_f ;
+        const long double y           = transformed_observation_y ;
+        const long double u_y           = map_landmarks.landmark_list[current_id].y_f ;
+        const long double x_ux_squared    = (x - u_x) * (x - u_x);
+        const long double y_uy_squared    = (y - u_y) * (y - u_y);
+        const long double part_2      = x_ux_squared / (standard_x_squared_2) +
+                              y_uy_squared / (standard_y_squared_2) ;
+        // long double exp_part_2        = exp( (-1/2) * (part_2)) ;  // ERROR
 
-					const long double x 				= transformed_observation_x ;
-					const long double u_x 				= map_landmarks.landmark_list[current_id].x_f ;
-					const long double y 			  	= transformed_observation_y ;
-					const long double u_y 		  		= map_landmarks.landmark_list[current_id].y_f ;
+        // cout << "transformed_observations\t" << x << "," << y << endl ;
+        // "\t reasonable_landmarks\t" << u_x << "," << u_y << endl ;
+        // cout << weight_placeholder << endl;
+        // cout << part_2 << "\t" << exp_part_2 << endl;
 
-					const long double x_ux_squared 		= (x - u_x) * (x - u_x) ;
-					const long double y_uy_squared 		= (y - u_y) * (y - u_y) ;
-					const long double part_2 			= x_ux_squared / (standard_x_squared_2) +
-										 		  		  y_uy_squared / (standard_y_squared_2) ;
-					long double exp_part_2				= exp( (-1/2) * (part_2)) ;
+        // long double w_i = part_1 * exp( (-1/2.) * (part_2)) ;
+        // weight_placeholder *= w_i ;
 
-					// cout << "transformed_observations\t" << x << "," << y << endl ;
-					// "\t reasonable_landmarks\t" << u_x << "," << u_y << endl ;
-					// cout << weight_placeholder << endl;
-					// cout << part_2 << "\t" << exp_part_2 << endl;
+        weight_placeholder *= part_1 * exp( (-1/2.) * (part_2));
 
-					weight_placeholder *= part_1 * exp_part_2 ;
-					// cout << weight_placeholder << endl;
-
-					// cout << "x " << x << "\tu_x " << u_x << "\ty " << y << "\tu_y " <<  u_y << endl ;
-				}
-			}
-		}
-		//weights[i] 			= weight_placeholder ;
-		particles[i].weight = weight_placeholder ;
-		weights.push_back(particles[i].weight);
-		// cout << "Weight update complete" << endl;
-		//cout << particles[i].x << "," << particles[i].y << ","  << particles[i].theta << endl;
-	}
+        // cout << weight_placeholder << endl;
+        // cout << "x " << x << "\tu_x " << u_x << "\ty " << y << "\tu_y " <<  u_y << endl ;
+      }
+    }
+    
+    //weights[i]      = weight_placeholder ;
+    particles[i].weight = weight_placeholder ;
+    weights.push_back(particles[i].weight);
+    // cout << "Weight update complete" << endl;
+    //cout << particles[i].x << "," << particles[i].y << ","  << particles[i].theta << endl;
+  }
 }
+
 
 void ParticleFilter::resample() {
 	// Resample particles with replacement with probability proportional to their weight.
