185f8039e756e95f9f4529f6805d1a920c01e12e
swirlingsand <anthonysarkis@gmail.com>
2017-05-18 10:08:06 -0600

    udacity starter code for quiz

---
+++ p10-model-predictive-control/quizes/global_kinematic_model.cpp
@@ -0,0 +1,41 @@
+// In this quiz you'll implement the global kinematic model.
+#include <math.h>
+#include <iostream>
+#include "Dense"
+
+//
+// Helper functions
+//
+double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+const double Lf = 2;
+
+// TODO: Implement the global kinematic model.
+// Return the next state.
+//
+// NOTE: state is [x, y, psi, v]
+// NOTE: actuators is [delta, a]
+Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
+                                Eigen::VectorXd actuators, double dt) {
+  Eigen::VectorXd next_state(state.size());
+
+  //TODO complete the next_state calculation ...
+
+  return next_state;
+}
+
+int main() {
+  // [x, y, psi, v]
+  Eigen::VectorXd state(4);
+  // [delta, v]
+  Eigen::VectorXd actuators(2);
+
+  state << 0, 0, deg2rad(45), 1;
+  actuators << deg2rad(5), 1;
+
+  Eigen::VectorXd next_state = globalKinematic(state, actuators, 0.3);
+
+  std::cout << next_state << std::endl;
+}
