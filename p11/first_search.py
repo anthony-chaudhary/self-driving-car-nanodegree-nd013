15f479f21e733549c39a61744ad61efc33238c8d
swirlingsand <anthonysarkis@gmail.com>
2017-07-07 11:55:45 -0600

    work in progress

--- p11/first_search.py
+++ p11/first_search.py
@@ -34,3 +34,42 @@ delta = [[-1, 0], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
+
+class node():
+
+    def __init__(self):
+        self.g = 0
+        self.x = None
+        self.y = None
+        self.e = False  # expanded
+        self.id = random.randint(1, 100)
+
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+
+    # 0. Init
+    a = node()
+    m = 0  # minimum distance
+    a.x, a.y = 4, 5  # first node
+    nodes = [a]
+
+    # 1. Find smallest node
+    for n in nodes:
+        if n.e is False:
+            if n.g <= m:
+                c = n  # c == current_node
+    
+    # 2. Check if at goal
+    if (c.x, c.y) == (goal[0], goal[1]):
+        return (c.g, c.x, c.y)
+    
+    # 3. Expand node
+
+    
+    return 0
+
+
+print(search(grid, init, goal, cost))
