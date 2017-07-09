89cd926ea8c015c2e873e02e8eb41fe5da8ccd0f
swirlingsand <anthonysarkis@gmail.com>
2017-07-09 15:25:52 -0600

    a*  and improve performance

--- p11/first_search.py
+++ p11/first_search.py
@@ -25,6 +25,18 @@ grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
+
+grid = [[0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]]
+
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 print(goal)
@@ -44,6 +56,7 @@ class node():
 
     def __init__(self):
         self.g = 0          # cost to get to node
+        self.f = 0    # A* 
         self.x = None
         self.y = None
         self.ep = False  # fully expanded
@@ -60,7 +73,7 @@ class space():
         self.ex = False  # Explored
 
 
-def search(grid,init,goal,cost):
+def search(grid,init,goal,cost, heuristic):
 
     # 0. Init
     expanded_grid = [[-1 for i in range(len(grid[0]))] for i in range(len(grid))]
@@ -69,6 +82,8 @@ def search(grid,init,goal,cost):
     a.x, a.y = init[0], init[1]  # first node
     expanded_grid[a.x][a.y] = a.id
     nodes = [a]
+    e_nodes = [a] # eligible nodes
+    costs = [a.f + a.g]
     explored_dict = {(a.x, a.y): "Explored"}
     expanded_dict = {}
     #print(expanded_grid)
@@ -76,28 +91,22 @@ def search(grid,init,goal,cost):
     while True:
 
         # 1. Find smallest cost node
-        print("\nNodes:", len(nodes), ", Nodes expanded:", len(expanded_dict))
+        print("\nNodes:", len(nodes), ", Nodes expanded:", len(expanded_dict), "Eligible nodes", len(e_nodes))
 
-        costs = []
-        for n in nodes:
-            if expanded_dict.get(n.id, False) is False:   # need to check here otherwise costs have invalid nodes
-                costs.append(n.g)
+        costs = [i.f for i in e_nodes]
         try:
             m = min(costs)
         except:
             exit("Fail")
-
         print("Lowest cost:", m, "Costs", costs)
 
-        c = None
-        for n in nodes:
-            if n.g <= m:
+        for n in e_nodes:
+            if n.f <= m:
                 if expanded_dict.get(n.id, False) is False:
                     print("id", n.id, "not in expanded_dict")
                     c = n  # c == current_node
-        print(c.id)
-        
         print("Node", c.id," at:", c.x, ",", c.y)
+
         # 2. Check if at goal
         if (c.x, c.y) == (goal[0], goal[1]):
             "At goal"
@@ -107,28 +116,28 @@ def search(grid,init,goal,cost):
         for d in delta:
             if (d[0], d[1]) == (0, 1):   # If we have reached last move, mark cell as expanded
                 expanded_dict.update({c.id: "Expanded"})
+                e_nodes.remove(c)
                 print("Added node", c.id, "position", c.x, ",", c.y, "to expanded.")
 
             x, y = c.x + d[0], c.y + d[1]   # do move
-
             if x >= 0 and y >=0:   # index sanity check, this should be better then a try block for speed
                 if x <= (len(grid)-1) and y <= len(grid[0])-1:
                 
                     explored = explored_dict.get((x, y), False)
                     if grid[x][y] == 0 and explored is False:   # if move is valid
                         new = node()
-                        new.g = c.g + 1    # update cost
+                        new.g = c.g + cost    # update cost
+                        new.f = new.g + heuristic[x][y]
                         print("New node:", new.id, "at", x, ",", y)
                         new.x, new.y = x, y
                         nodes.append(new)
-
+                        e_nodes.append(new)
                         expanded_grid[x][y] = new.g
-
                         explored_dict.update({(x, y): "Explored"})
                         break
 
 
-nodes, result, expanded_grid = search(grid, init, goal, cost)
+nodes, result, expanded_grid = search(grid, init, goal, cost, heuristic)
 nodes.sort(key=lambda x: x.g)
 #set(nodes)
 #previous_cost = 0
