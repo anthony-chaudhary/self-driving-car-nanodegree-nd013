9b8f4665746c7cc022d81d400a455463c0734e6e
swirlingsand <anthonysarkis@gmail.com>
2017-07-10 13:33:39 -0600

    add n scale testing

--- p11/first_search.py
+++ p11/first_search.py
@@ -30,13 +30,41 @@ grid = [[0, 0, 0, 0, 0, 0],
         [0, 1, 1, 1, 1, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]]
+        [0, 0, 0, 0, 1, 0]]
 
-heuristic = [[9, 8, 7, 6, 5, 4],
-             [8, 7, 6, 5, 4, 3],
-             [7, 6, 5, 4, 3, 2],
-             [6, 5, 4, 3, 2, 1],
-             [5, 4, 3, 2, 1, 0]]
+
+x = 20
+y = 1000
+
+grid = [[0 for x in range(x)] for y in range(y)]
+grid[1][1] = 1
+grid[2][2] = 1
+grid[4][4] = 1
+#grid[7][7] = 1
+for i in range(3):
+    grid[i][0] = 1
+
+#for i in grid:
+    #print(i)
+
+
+heuristic = [[0 for x in range(x)] for y in range(y)]
+
+# populate heuristic
+for i in range(y):
+    for ii in range(x):
+        heuristic[i][ii] = y - i + x - ii - 2
+    
+#heuristic = [[9, 8, 7, 6, 5, 4],
+#             [8, 7, 6, 5, 4, 3],
+#             [7, 6, 5, 4, 3, 2],
+#             [6, 5, 4, 3, 2, 1],
+#             [5, 4, 3, 2, 1, 0]]
+
+#print()
+#for i in heuristic:
+    #print(i)
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 print(goal)
@@ -91,25 +119,25 @@ def search(grid,init,goal,cost, heuristic):
     while True:
 
         # 1. Find smallest cost node
-        print("\nNodes:", len(nodes), ", Nodes expanded:", len(expanded_dict), "Eligible nodes", len(e_nodes))
+        #print("\nNodes:", len(nodes), ", Nodes expanded:", len(expanded_dict), "Eligible nodes", len(e_nodes))
 
         costs = [i.f for i in e_nodes]
         try:
             m = min(costs)
         except:
             exit("Fail")
-        print("Lowest cost:", m, "Costs", costs)
+        #print("Lowest cost:", m, "Costs", costs)
 
         for n in e_nodes:
             if n.f <= m:
                 if expanded_dict.get(n.id, False) is False:
-                    print("id", n.id, "not in expanded_dict")
+                    #print("id", n.id, "not in expanded_dict")
                     c = n  # c == current_node
-        print("Node", c.id," at:", c.x, ",", c.y)
+        #print("Node", c.id," at:", c.x, ",", c.y)
 
         # 2. Check if at goal
         if (c.x, c.y) == (goal[0], goal[1]):
-            "At goal"
+            #"At goal"
             return nodes, [c.g, c.x, c.y], expanded_grid
 
         # 3. Expand node if possible
@@ -117,7 +145,7 @@ def search(grid,init,goal,cost, heuristic):
             if (d[0], d[1]) == (0, 1):   # If we have reached last move, mark cell as expanded
                 expanded_dict.update({c.id: "Expanded"})
                 e_nodes.remove(c)
-                print("Added node", c.id, "position", c.x, ",", c.y, "to expanded.")
+                #print("Added node", c.id, "position", c.x, ",", c.y, "to expanded.")
 
             x, y = c.x + d[0], c.y + d[1]   # do move
             if x >= 0 and y >=0:   # index sanity check, this should be better then a try block for speed
@@ -128,7 +156,7 @@ def search(grid,init,goal,cost, heuristic):
                         new = node()
                         new.g = c.g + cost    # update cost
                         new.f = new.g + heuristic[x][y]
-                        print("New node:", new.id, "at", x, ",", y)
+                        #print("New node:", new.id, "at", x, ",", y)
                         new.x, new.y = x, y
                         nodes.append(new)
                         e_nodes.append(new)
@@ -152,8 +180,8 @@ def path(expanded_grid):
 
     path_grid = [[" " for i in range(len(grid[0]))] for i in range(len(grid))]
     a = node()
-    a.x, a.y = 4, 5
-    a.p = None   # previous
+    a.x, a.y = goal[0], goal[1]
+    a.p = None  # previous
     path_grid[result[1]][result[2]] = "*"
     l = result[0]
     for i in range(l):
