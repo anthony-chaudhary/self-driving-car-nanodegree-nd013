63cf501aeb595c4a42fe72f4157dfac1995215be
swirlingsand <anthonysarkis@gmail.com>
2017-07-11 11:54:36 -0600

    wip

--- p11/left_turn_policy.py
+++ p11/left_turn_policy.py
@@ -22,17 +22,17 @@ grid = [[1, 1, 1, 0, 0, 0],
         [1, 1, 1, 0, 1, 1],
         [1, 1, 1, 0, 1, 1]]
 
-goal = [2, 0] # given in the form [row,col]
+goal = [0, 2] # given in the form [row,col]
 print("\n Goal:", goal)
 
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+delta = [[0, -1], # go up
+         [ -1, 0], # go left
+         [ 0, 1], # go down
+         [ 1, 0]] # go right
 
-delta_name = ['v', '>', '^', '<']
+delta_name = ['^', '<', 'V', '>']
 
-init = [4, 3, 0]
+init = [3, 4, 0]
 
 class action():
 
@@ -53,8 +53,19 @@ class node():
         self.y = None
         self.ep = False  # fully expanded
         self.p = None   # previous node
+        self.path = None   # path
         self.d = 0   # delta action 
         self.id = node.newid.__next__()
+
+
+class path():
+    
+    newid = itertools.count()
+
+    def __init__(self):
+        self.id = path.newid.__next__()
+        self.nodes = []
+        self.g = 0   # running cost
 
 
 class space():
@@ -72,25 +83,40 @@ right.o, forward.o, left.o = -1, 0 ,1
 right.n, forward.n, left.n = "right", "forward", "left"
 
 
+def debug(grid, x, y, o):
+    
+    for i in range(len(grid)):
+        for ii in range(len(grid[i])):
+            if i == y and ii == x:
+                print(" {} ".format(delta_name[o]), end="")
+            else:
+                print(" - ", end="")
+        print()
+
 def search(grid, init, goal):
 
     # 0. Init
     expanded_grid = [[[node()] for i in range(len(grid[0]))] for i in range(len(grid))]
 
-    a = node()
-    a.x, a.y = init[0], init[1]  # first node
-    expanded_grid[a.x][a.y] = [a]
-    e_nodes = [a] # eligible nodes
+    n = node()
+    n.x, n.y = init[0], init[1]  # first node
+    expanded_grid[n.x][n.y] = [n]
+    e_nodes = [n] # eligible nodes
+    debug(grid, n.x, n.y, n.o)
 
-    for i in range(30):
+    p = path()
+    p.nodes.append(n)
+    path_dict = {n: p}  # map node to path
+    paths = [p] 
 
-        print(len(e_nodes))
+    for i in range(15):
+
+        print("Length of e_nodes", len(e_nodes))
         for n in e_nodes:  # get next eligible nodes
             c = n  # c == current_node
+            e_nodes.remove(n)
             break
 
-        e_nodes.remove(c)
-        
         # 2. Check if at goal
         if (c.x, c.y) == (goal[0], goal[1]):
             return expanded_grid
@@ -98,32 +124,50 @@ def search(grid, init, goal):
         # 3. Expand node if possible
         valid_actions = []
         for a in actions:
-
             o = c.o + a.o   # Update car orientation based on action orientation
             d = delta[o]  # get move based on car orientation
             x, y = c.x + d[0], c.y + d[1]   # do move
             if x >= 0 and y >=0 and x <= (len(grid)-1) and y <= len(grid[0])-1:  # check if valid move.
-                if grid[x][y] == 0:
+                if grid[y][x] == 0:
                     print(d, o)  
-                    valid_actions.append([x, y, a, o])
+                    a.o = o
+                    debug(grid, x, y, o)
+                    valid_actions.append([x, y, a])
            
-        branches = []        
+        branches = []
+        l = len(valid_actions)
+        print("Valid actions", l)
+        #print(path_dict)
+
         for v in valid_actions:
 
             x, y = v[0],v[1]
             n = node()
             n.g = c.g + v[2].g                  
             n.x, n.y = x, y
+            n.p = c.id
+            n.o = v[2].o   # Update nodes oreintation
             e_nodes.append(n)
             branches.append(n)
-            n.o = v[3]
-        
-        expanded_grid[x][y] = branches
 
-    return expanded_grid
+            if path_dict.get(c, False) not False:
+                p_prior = path_dict[c] # previous path
+                p_prior.nodes.append(n)
+                p_prior.g += n.g
+                path_dict.update({n: p_prior})
+            else:
+                p = path()  # spawn new path
+                p.nodes = p_prior.nodes  # copy previous nodes
+                p.nodes.append(n)  # add current node
+                p.g = p_prior.g + n.g   # update running cost for path
+                paths.append(p)
+                print("Length of paths", len(paths))
+                path_dict.update({n: p}) # update dictionary
+
+        expanded_grid[y][x] = branches
 
 
-def path(expanded_grid):
+def path_2(expanded_grid):
 
     path_grid = [[" " for i in range(len(grid[0]))] for i in range(len(grid))]
     a = node()
@@ -150,11 +194,13 @@ path_grid = expanded_grid
 #path_grid = path(expanded_grid)
 print()
 
-print("\nCosts:")
+print("\nNodes:")
 for i, x in enumerate(expanded_grid):
     for y in expanded_grid[i]:
         print("{x:2d}".format(x=len(y) ), " ", end="")
     print()
+
+
 
 #print("\nPaths")
 #for i, x in enumerate(path_grid):
