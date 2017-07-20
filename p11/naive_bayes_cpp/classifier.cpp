64e4d43075c07e4394a59eb4991bd52e7bd2e177
swirlingsand <anthonysarkis@gmail.com>
2017-07-20 15:57:28 -0600

    basic naive bayes wip

--- p11/naive_bayes_cpp/classifier.cpp
+++ p11/naive_bayes_cpp/classifier.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+
+
 #include <math.h>
 #include <vector>
 #include "classifier.h"
@@ -33,6 +35,53 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 	labels - array of N labels
 	- Each label is one of "left", "keep", or "right".
 	*/
+
+	//
+	// int n_features = data.size();
+	// int n_classes = possible_labels.size();
+	// theta_ = matrix()
+
+	double len_labels = labels.size();
+	cout << "\nLen_labels " << len_labels << endl;
+
+	// chance of being left
+	int left_count = 0;
+	double left_data_sum = 0;
+	vector<double> left_data_variance;
+
+	for (size_t i = 0; i < len_labels; ++i) {
+		if (labels[i] == "left") {
+			left_count += 1;
+			left_data_sum += data[i][0];
+			left_data_variance.push_back(data[i][0]);
+		}
+	}
+
+	double left_chance = left_count / len_labels;
+	double left_data_mean = left_data_sum / left_count;
+
+	double left_data_variance_sum = 0;
+	for (size_t i = 0; i < left_data_variance.size(); ++i) {
+		double difference = left_data_mean - left_data_variance[i];
+		double square = pow(difference, 2);
+		left_data_variance_sum += square;
+	}
+	double left_data_variance_mean = left_data_variance_sum / left_count;
+
+	cout << "Left chance " << left_chance << " left_count " << left_count << endl;
+	cout << "left_data_mean " << left_data_mean << " left_data_sum " << left_data_sum << endl;
+	cout << "left_data_variance_mean " << left_data_variance_mean << " left_data_variance_sum " << left_data_variance_sum << endl;
+
+	double x = left_data_variance[0];
+	double mean_y = left_data_mean;
+	double variance_y = left_data_variance_mean;
+	double p = 1 / sqrt(2 * M_PI * variance_y) 
+		* exp( (- pow(x - mean_y, 2) ) / (2 * variance_y) );
+
+	double posterior = left_chance * p;
+
+	cout << "posterior " << posterior << endl;
+	cout << endl;
 }
 
 string GNB::predict(vector<double>)
