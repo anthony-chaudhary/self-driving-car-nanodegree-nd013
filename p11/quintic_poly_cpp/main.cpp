b4219f554d68b14e1aef5508c7ec5ad7ff4852e1
swirlingsand <anthonysarkis@gmail.com>
2017-07-20 10:46:27 -0600

    complete quiz
    (grader has bug, but results are correct)

--- p11/quintic_poly_cpp/main.cpp
+++ p11/quintic_poly_cpp/main.cpp
@@ -3,8 +3,7 @@
 #include <cmath>
 #include <vector>
 
-#include "Eigen-3.3/Eigen/Core"
-#include "Eigen-3.3/Eigen/QR"
+#include "Eigen-3.3/Eigen/Dense"
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -36,7 +35,45 @@ vector<double> JMT(vector< double> start, vector <double> end, double T)
     > JMT( [0, 10, 0], [10, 10, 0], 1)
     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
     */
-    return {1,2,3,4,5,6};
+
+	const double s_i				= start[0] ;
+	const double s_i_dot			= start[1] ;
+	const double s_i_dot_dot		= start[2] / 2;
+
+	const double s_f				= end[0];
+	const double s_f_dot			= end[1];
+	const double s_f_dot_dot		= end[2];
+
+	const double s_r_1 = s_f - (s_i + s_i_dot * T + (s_i_dot_dot * T * T) / 2);
+	const double s_r_2 = s_f_dot - (s_i_dot + s_i_dot_dot * T);
+	const double s_r_3 = s_f_dot_dot - s_i_dot_dot;
+
+	VectorXd t_1(3), t_2(3), t_3(3);
+	t_1 << pow(T, 3), pow(T, 4), pow(T, 5);
+	t_2 << 3 * pow(T, 2), 4 * pow(T, 3), 5 * pow(T, 4);
+	t_3 << 6 * pow(T, 1), 12 * pow(T, 2), 20 * pow(T, 3);
+
+	MatrixXd s_matrix(3,1), T_matrix(3,3);
+	s_matrix << s_r_1, s_r_2, s_r_3;
+	T_matrix.row(0) = t_1;
+	T_matrix.row(1) = t_2;
+	T_matrix.row(2) = t_3;
+
+	MatrixXd T_inverse = T_matrix.inverse();
+
+	MatrixXd output(3,1);
+	output = T_inverse * s_matrix;
+
+	double a_3 = double(output.data()[0]);
+	double a_4 = double(output.data()[1]);
+	double a_5 = double(output.data()[2]);
+
+	vector<double> results;
+	results = { s_i, s_i_dot, s_i_dot_dot, a_3, a_4, a_5 };
+
+	cout << output << endl;
+
+    return results;
     
 }
 
