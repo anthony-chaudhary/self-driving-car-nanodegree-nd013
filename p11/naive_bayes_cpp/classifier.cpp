6a56c7735a91483bf31cf5326d5225097b72630b
swirlingsand <anthonysarkis@gmail.com>
2017-07-20 16:46:58 -0600

    refactor to handle multiple labels

--- p11/naive_bayes_cpp/classifier.cpp
+++ p11/naive_bayes_cpp/classifier.cpp
@@ -45,42 +45,89 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 	cout << "\nLen_labels " << len_labels << endl;
 
 	// chance of being left
-	int left_count = 0;
-	double left_data_sum = 0;
-	vector<double> left_data_variance;
+	vector<double> data_counts = { 0, 0 ,0 };
+	vector<double> data_sums = { 0, 0, 0 };
+	vector <vector<double> > data_variance;
+	data_variance.resize(3);
 
+	// refactored for all labels 
+	// TODO do more than first data element
 	for (size_t i = 0; i < len_labels; ++i) {
+		int index;
 		if (labels[i] == "left") {
-			left_count += 1;
-			left_data_sum += data[i][0];
-			left_data_variance.push_back(data[i][0]);
+			index = 0;
 		}
+		if (labels[i] == "keep") {
+			index = 1;
+		}
+		if (labels[i] == "right") {
+			index = 2;
+		}
+		data_counts[index] += 1;
+		data_sums[index] += data[i][0];
+		data_variance[index].push_back(data[i][0]);
+
 	}
 
-	double left_chance = left_count / len_labels;
-	double left_data_mean = left_data_sum / left_count;
+	cout << "Updating variables\n" << endl;
+	// update variables
+	vector <vector <double> >results;
+	results.resize(3);
+	for (size_t i = 0; i < data_counts.size(); ++i) {
+		
+		cout << possible_labels[i] << endl;
+		double chance = data_counts[i] / len_labels;
+		double data_mean = data_sums[i] / data_counts[i];
 
-	double left_data_variance_sum = 0;
-	for (size_t i = 0; i < left_data_variance.size(); ++i) {
-		double difference = left_data_mean - left_data_variance[i];
-		double square = pow(difference, 2);
-		left_data_variance_sum += square;
+		double data_variance_sum = 0;
+		for (size_t j = 0; j < data_variance[i].size(); ++j) {
+			double difference = data_mean - data_variance[i][j];
+			double square = pow(difference, 2);
+			data_variance_sum += square;
+		}
+
+		double data_variance_mean = data_variance_sum / data_counts[i];
+
+		results[i].push_back(chance);
+		results[i].push_back(data_mean);
+		results[i].push_back(data_variance_mean);
+
+		cout << "Chance " << chance << " count " << data_counts[i] << endl;
+		cout << "data_mean " << data_mean << " data_sum " << data_sums[i] << endl;
+		cout << "data_variance_mean " << data_variance_mean <<
+			" data_variance_sum " << data_variance_sum << "\n" << endl;
+
 	}
-	double left_data_variance_mean = left_data_variance_sum / left_count;
 
-	cout << "Left chance " << left_chance << " left_count " << left_count << endl;
-	cout << "left_data_mean " << left_data_mean << " left_data_sum " << left_data_sum << endl;
-	cout << "left_data_variance_mean " << left_data_variance_mean << " left_data_variance_sum " << left_data_variance_sum << endl;
-
-	double x = left_data_variance[0];
-	double mean_y = left_data_mean;
-	double variance_y = left_data_variance_mean;
+	double x = data[1][0];
+	double mean_y = results[0][1];
+	double variance_y = results[0][2];
 	double p = 1 / sqrt(2 * M_PI * variance_y) 
 		* exp( (- pow(x - mean_y, 2) ) / (2 * variance_y) );
 
-	double posterior = left_chance * p;
+	double posterior = results[0][0] * p;
 
-	cout << "posterior " << posterior << endl;
+	cout << "posterior " << posterior << " for " << possible_labels[0] << endl;
+	cout << endl;
+
+	mean_y = results[1][1];
+	variance_y = results[1][2];
+	p = 1 / sqrt(2 * M_PI * variance_y)
+		* exp((-pow(x - mean_y, 2)) / (2 * variance_y));
+
+	posterior = results[1][0] * p;
+
+	cout << "posterior " << posterior << " for " << possible_labels[1] << endl;
+	cout << endl;
+
+	mean_y = results[2][1];
+	variance_y = results[2][2];
+	p = 1 / sqrt(2 * M_PI * variance_y)
+		* exp((-pow(x - mean_y, 2)) / (2 * variance_y));
+
+	posterior = results[2][0] * p;
+
+	cout << "posterior " << posterior << " for " << possible_labels[2] << endl;
 	cout << endl;
 }
 
